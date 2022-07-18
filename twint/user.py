2652839b51555f1f9d01778baf9c1227a3bced4d
Jerrylee Isaac Geronimo <jerrylee@thinkingmachin.es>
2022-07-18 16:19:53 +0800

    Add default for getting fields

--- twint/user.py
+++ twint/user.py
@@ -24,31 +24,31 @@ def User(ur):
         raise KeyError(msg)
     _usr = user()
     _usr.id = ur['data']['user']['rest_id']
-    _usr.name = ur['data']['user']['legacy']['name']
-    _usr.username = ur['data']['user']['legacy']['screen_name']
-    _usr.bio = ur['data']['user']['legacy']['description']
-    _usr.location = ur['data']['user']['legacy']['location']
-    _usr.url = ""
-    if "url" in ur['data']['user']['legacy']:
-        _usr.url = ur['data']['user']['legacy']['url']
+    legacy_data = ur['data']['user']['legacy'] 
+
+    _usr.name = legacy_data.get("name", "")
+    _usr.username = legacy_data.get("username", "")
+    _usr.bio = legacy_data.get("description", "")
+    _usr.location = legacy_data.get('location', "")
+    _usr.url = legacy_data.get("url", "")
     # parsing date to user-friendly format
-    _dt = ur['data']['user']['legacy']['created_at']
+    _dt = legacy_data.get('created_at')
     _dt = datetime.datetime.strptime(_dt, '%a %b %d %H:%M:%S %z %Y')
     # date is of the format year,
     _usr.join_date = _dt.strftime(User_formats['join_date'])
     _usr.join_time = _dt.strftime(User_formats['join_time'])
 
     # :type `int`
-    _usr.tweets = int(ur['data']['user']['legacy']['statuses_count'])
+    _usr.tweets = int(legacy_data.get('statuses_count'))
     _usr.following = int(ur['data']['user']['legacy']['friends_count'])
     _usr.followers = int(ur['data']['user']['legacy']['followers_count'])
     _usr.likes = int(ur['data']['user']['legacy']['favourites_count'])
     _usr.media_count = int(ur['data']['user']['legacy']['media_count'])
 
-    _usr.is_private = ur['data']['user']['legacy']['protected']
-    _usr.is_verified = ur['data']['user']['legacy']['verified']
-    _usr.avatar = ur['data']['user']['legacy']['profile_image_url_https']
-    _usr.background_image = ur['data']['user']['legacy']['profile_banner_url']
+    _usr.is_private = legacy_data.get('protected', "")
+    _usr.is_verified = legacy_data.get('verified', "")
+    _usr.avatar = legacy_data.get('profile_image_url_https', "")
+    _usr.background_image = legacy_data.get('profile_banner_url', "")
     # TODO : future implementation
     # legacy_extended_profile is also available in some cases which can be used to get DOB of user
     return _usr
